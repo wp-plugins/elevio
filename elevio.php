955f902fa6a37b8041816d2855652e9c2f9caa61
elevio <elevio@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-22 06:35:39 +0000

    fix to secret id retrieval
    git-svn-id: https://plugins.svn.wordpress.org/elevio/trunk@1011917 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- elevio.php
+++ elevio.php
@@ -5,7 +5,7 @@ Plugin URI: https://elevio.com/integrations/wordpress/
 Description: Display your whole knowledge base and chat in a single tab on every page of your site.
 Author: Elevio
 Author URI: https://elev.io
-Version: 3.0.0
+Version: 3.0.1
 */
 
 if (is_admin())
