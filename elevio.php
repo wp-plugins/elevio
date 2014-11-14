9c88a5fe0faf40827cf327661743e18c0835bb99
elevio <elevio@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-14 05:13:24 +0000

    move scripts to footer
    git-svn-id: https://plugins.svn.wordpress.org/elevio/trunk@1025524 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- elevio.php
+++ elevio.php
@@ -5,7 +5,7 @@ Plugin URI: https://elev.io/
 Description: A better way for your users to access the help they need.
 Author: Elevio
 Author URI: https://elev.io
-Version: 3.1.0
+Version: 3.1.1
 */
 
 if (is_admin())
